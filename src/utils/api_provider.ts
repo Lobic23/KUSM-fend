a1bddf0cea0ef06ed86b41a337a5c25508093c4b
Rasaili-rain <saditrasaili063.com>
2026-01-18 20:32:17 +0545

    where is ur api url? dont need it

--- src/utils/api_provider.ts
+++ src/utils/api_provider.ts
@@ -1,9 +1,9 @@
 import { useAuthStore } from "@/stores/authStore";
 import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
-if (!apiUrl) throw new Error("Missing VITE_API_URL in .env");
+// if (!apiUrl) console.warn("Missing VITE_API_URL in .env");
 
 // Create axios instance
 export const axiosInstance = axios.create({
